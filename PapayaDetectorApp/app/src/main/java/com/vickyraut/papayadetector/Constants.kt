e125afa93e198b0d337891fbb547a080488dbe80
Codemonk <vraut12cr@gmail.com>
2024-05-11 00:28:53 +0530

    First Commit

---
+++ PapayaDetectorApp/app/src/main/java/com/vickyraut/papayadetector/Constants.kt
@@ -0,0 +1,6 @@
+package com.vickyraut.papayadetector
+
+object Constants {
+    const val MODEL_PATH = "papayaModel.tflite"
+    const val LABELS_PATH = "labels.txt"
+}
---
+++ README.dataset.txt
@@ -0,0 +1,6 @@
+# Greebel > 2022-12-05 11:51am
+https://universe.roboflow.com/unib/greebel
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
